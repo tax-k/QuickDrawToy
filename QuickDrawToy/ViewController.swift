f863f631a75b86c72e4f2a908f521b4beb98f2be
tax_k <taxfree9090@gmail.com>
2019-02-02 20:38:00 +0900

    MAKE simple Draw app using UIGraphic

--- QuickDrawToy/ViewController.swift
+++ QuickDrawToy/ViewController.swift
@@ -8,13 +8,63 @@
 
 import UIKit
 
+class Canvas:UIView {
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(8)
+        context.setLineCap(CGLineCap.butt)
+        
+        lines.forEach { (line) in
+            for (idx, value) in line.enumerated() {
+                if idx == 0 {
+                    context.move(to: value)
+                }else {
+                    context.addLine(to: value)
+                }
+            }
+            
+        }
+        
+        context.strokePath()
+    }
+    
+    var lines = [[CGPoint]]()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else {
+            return
+        }
+        
+        
+        guard var lastLine = lines.last else { return }
+        lastLine.append(point)
+        lines.append(lastLine)
+        
+        setNeedsDisplay()
+    }
+}
+
 class ViewController: UIViewController {
+    
+    let canvas = Canvas()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(canvas)
+        canvas.backgroundColor = .white
+        canvas.frame = view.frame
     }
-
 
 }
 
